e3d6079ce197b6abf1984781e0191f4760dbffc0
Kirill Pavlishin <kirill1999super15@gmail.com>
2023-03-03 02:35:04 +0300

    Implemented the output of messages using the python-cowsay module

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -3,6 +3,7 @@ import random
 import sys
 import validators
 from urllib import request as req
+import cowsay
 
 
 def bullscows(guess, secret):
@@ -21,28 +22,35 @@ def gameplay(ask, inform, words):
     computer_word = random.choice(words)
     nattempts = 1
 
-    user_word = ask(f'Введите слово длиной {args.length}: ', words)
+    user_word = ask(f'Введите слово длиной {args.length}', words)
     while user_word != computer_word:
         inform('Быки: {}, Коровы: {}', *bullscows(user_word, computer_word))
         nattempts += 1
-        user_word = ask(f'Введите слово длиной {args.length}: ', words)
+        user_word = ask(f'Введите слово длиной {args.length}', words)
 
     return nattempts
 
 
 def ask(prompt, valid=None):
-    word = input(prompt)
+    cow = random.choice(cowsay.list_cows())
+    word = input(cowsay.cowsay(prompt, cow=cow) + '\n\n')
+    print()
 
     if valid is not None:
         while word not in valid:
-            print('Введённого слова нет в словаре')
-            word = input(prompt)
+            cow = random.choice(cowsay.list_cows())
+            print(cowsay.cowsay('Введённого слова нет в словаре', cow=cow))
+            print()
+            word = input(cowsay.cowsay(prompt, cow=cow) + '\n\n')
+            print()
 
     return word
 
 
 def inform(format_string, bulls, cows):
-    print(format_string.format(bulls, cows))
+    cow = random.choice(cowsay.list_cows())
+    print(cowsay.cowsay(format_string.format(bulls, cows), cow=cow))
+    print()
 
 
 if __name__ == '__main__':
@@ -50,6 +58,8 @@ if __name__ == '__main__':
     parser.add_argument('dict', type=str)
     parser.add_argument('length', nargs='?', default=5, type=int)
     args = parser.parse_args()
+
+    cow = random.choice(cowsay.list_cows())
 
     if validators.url(args.dict):
         with req.urlopen(args.dict) as file:
@@ -63,7 +73,9 @@ if __name__ == '__main__':
             sys.exit()
 
     if not dictionary:
-        print('Запустите игру с меньшей длиной слова')
+        print(cowsay.cowsay('Запустите игру с меньшей длиной слова', cow=cow))
+        print()
         sys.exit()
 
-    print(f'Вы угадали слово за {gameplay(ask, inform, dictionary)} попыток')
+    print(cowsay.cowsay(f'Вы угадали слово за {gameplay(ask, inform, dictionary)} попыток', cow=cow))
+    print()
